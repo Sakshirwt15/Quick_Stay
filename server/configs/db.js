1e91422e8cfccdfc84038978f1e94c00a40468d9
Sakshirwt15 <156641221+Sakshirwt15@users.noreply.github.com>
2025-07-14 11:15:43 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("database connected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+    await mongoose.connect(process.env.MONGODB_URI)
+
 
   } catch (error) {
     console.log(error.message);
